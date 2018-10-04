66951ec0976833ff168b99bee08bb7a090f2a1fa
Navjot Singh <navisinghnz9@gmail.com>
2018-10-05 12:46:44 +1300

    using sanity to get categories

--- README.md
+++ README.md
@@ -25,3 +25,12 @@ npm run test
 ```
 npm run lint
 ```
+
+### Sanity Client
+- Santiy client is used to get data via sanity APIs. [Check sanity client docs](https://www.sanity.io/docs/client-libraries/js-client)
+- Setup an environment variable for sanity's access token, named as SANITY_TODO_ACCESS_TOKEN, as use it in sanity.js
+- You need to enable CORS for your vue frontend: 
+```
+sanity cors add http://localhost:8080
+```
+
--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -2,9 +2,9 @@
     <div class="container">
         <ul class="list-group" v-for="category in categories">
             <li class="list-group-item" 
-                v-bind:class="{active: category == selectedCategory}"
-                @click="changeCategory(category)">
-                {{ category }}
+                v-bind:class="{active: category.title == selectedCategory}"
+                @click="changeCategory(category.title)">
+                {{ category.title }}
             </li>
         </ul>
     </div>
@@ -12,27 +12,39 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import sanity from '../sanity';
 
 @Component({})
 export default class CategoryList extends Vue {
 
     private selectedCategory: string;
-
-
-    private categories: string[] = [
-        'Shopping',
-        'Study',
-        'Wintec study',
-        'Farmax work',
-        'Hangout',
-        'Self improvement',
-        'Friends',
-        'Family',
-    ];
+    private categories: any[];
 
     constructor() {
         super();
-        this.selectedCategory = this.categories[0];
+
+        this.selectedCategory = "";
+        this.categories = [];
+
+        this.getAllCategories();
+    }
+
+    private getAllCategories() {
+
+        const query = `*[_type == "category"]`;
+
+        this.categories = [];
+
+        sanity.fetch(query, {}).then(categories => {
+            this.categories = categories;
+
+            // if no category is selected, select first one as default
+            if(this.selectedCategory == "" && this.categories.length > 0) {
+                this.selectedCategory = categories[0].title;
+            }
+        }, error => {
+            console.log("got error !");
+        });
     }
 
     private changeCategory(category: string) {
